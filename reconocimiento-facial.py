acbb1d44cbc8144d0e9631e887d4e37f658bd321
SHuertasRo <88412486+SamHuRo@users.noreply.github.com>
2024-09-28 14:24:57 -0500

    update

--- reconocimiento-facial.py
+++ reconocimiento-facial.py
@@ -6,11 +6,16 @@ import numpy as np
 print("Estoy funcionando")
 
 #Cargar imagenes
-foto_control = fr.load_image_file('C:\Users\samhu\Documents\controlador-de-asistencia\Imagenes\FotoA.jpg')
-foto_prueba = fr.load_image_file('C:\Users\samhu\Documents\controlador-de-asistencia\Imagenes\FotoB.jpg')
+foto_control = fr.load_image_file('Imagenes/FotoA.jpg')
+foto_prueba = fr.load_image_file('Imagenes/FotoB.jpg')
 
 #Cambiar el formato de las imagenes
 foto_control = cv2.cvtColor(foto_control, cv2.COLOR_BGR2RGB)
-foto_prueba = cv2.cvtColor(foto_control, cv2.COLOR_BGR2RGB)
+foto_prueba = cv2.cvtColor(foto_prueba, cv2.COLOR_BGR2RGB)
 
 #Mostrar Imagenes
+cv2.imshow('Foto Control', foto_control)
+cv2.imshow('Foto Prueba', foto_prueba)
+
+#Mantener el programa abierto
+cv2.waitKey(0)
