949e8e2548dbe6072e9092fb1d1c9b3c38dbc2ca
SHuertasRo <88412486+SamHuRo@users.noreply.github.com>
2024-09-28 14:33:20 -0500

    update

--- reconocimiento-facial.py
+++ reconocimiento-facial.py
@@ -13,6 +13,31 @@ foto_prueba = fr.load_image_file('Imagenes/FotoB.jpg')
 foto_control = cv2.cvtColor(foto_control, cv2.COLOR_BGR2RGB)
 foto_prueba = cv2.cvtColor(foto_prueba, cv2.COLOR_BGR2RGB)
 
+#Localizar la cara de la foto control
+lugar_cara_a = fr.face_locations(foto_control)[0]
+cara_codificada_a = fr.face_encodings(foto_control)[0]
+
+lugar_cara_b = fr.face_locations(foto_prueba)[0]
+cara_codificada_b = fr.face_encodings(foto_prueba)[0]
+
+#Mostrar rectangulo
+#print(lugar_cara_a)
+cv2.rectangle(foto_control, #Imagen donde dibujar rectangulo
+              (lugar_cara_a[3], lugar_cara_a[0]), #Punto superior del rectangulo
+              (lugar_cara_a[1], lugar_cara_a[2]), #Punto inferior del rectangulo
+              (0,255,0), #Color del rectangulo
+              2 #Grosor del borde del rectangulo
+)
+
+cv2.rectangle(foto_prueba, #Imagen donde dibujar rectangulo
+              (lugar_cara_b[3], lugar_cara_b[0]), #Punto superior del rectangulo
+              (lugar_cara_b[1], lugar_cara_b[2]), #Punto inferior del rectangulo
+              (0,255,0), #Color del rectangulo
+              2 #Grosor del borde del rectangulo
+)
+
+
+
 #Mostrar Imagenes
 cv2.imshow('Foto Control', foto_control)
 cv2.imshow('Foto Prueba', foto_prueba)
