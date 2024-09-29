8d1a437672b75ac0f1c0d134ffedf9cb45ce99ca
SHuertasRo <88412486+SamHuRo@users.noreply.github.com>
2024-09-28 19:14:34 -0500

    update

--- asistencia.py
+++ asistencia.py
@@ -30,3 +30,49 @@ def codificar(imagenes):
     return lista_codificada
 
 lista_empleados_codificada = codificar(mis_imagenes)
+
+#Tomar una imagen de la camara web
+captura = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+#Leer la imagen de la camara
+exito, imagen = captura.read()
+
+if not exito:
+    print('No se tomo la captura')
+else:
+    #Reconocer cara en la captura
+    cara_captura = fr.face_locations(imagen)
+
+    #Codificar la cara capturada
+    cara_captura_codificada = fr.face_encodings(imagen, cara_captura)
+
+    #Buscar las coicidencias entre la lista de empleados
+    for caracodif, caraubic in zip(cara_captura_codificada, cara_captura):
+
+        coicidencias = fr.compare_faces(lista_empleados_codificada, caracodif)
+        distancias = fr.face_distance(lista_empleados_codificada, caracodif)
+
+        print(distancias)
+
+        indice_coicidencia = np.argmin(distancias)
+
+        #Mostrar coicidencias
+        if distancias[indice_coicidencia] > 0.6:
+            print('NO es un empleado')
+        else:
+            #Buscar el nombre del empleado encontrado
+            nombre = nombres_empleados[indice_coicidencia]
+
+            #Mostrar el rectangulo de la imagen
+            y1, x2, x1, y2 = caraubic
+
+            #Crear el rectangulo
+            cv2.rectangle(imagen, (x1, y1), (x2, y2), (0,255,0), 2)
+            cv2.rectangle(imagen, (x1, y2 - 35), (x2, y2), (0,255,0), cv2.FILLED)
+            cv2.putText(imagen, nombre, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 2)
+
+            #Mostrar la imagen obtenida
+            cv2.imshow('Imagen web', imagen)
+            #Mantener la ventana abierta
+            cv2.waitKey(0)
+            print(f'Bienvendio {nombre}')
