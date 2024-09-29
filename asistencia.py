6f0f114998e3ce37dee1d6015369be7628118abd
SHuertasRo <88412486+SamHuRo@users.noreply.github.com>
2024-09-28 19:24:10 -0500

    update

--- asistencia.py
+++ asistencia.py
@@ -32,7 +32,18 @@ def codificar(imagenes):
 
 #Registrar ingresos
 def registrar_ingresos(persona):
-    return True
+    f = open('registro.csv', 'r+')
+    lista_datos = f.readlines()
+    nombre_registro = []
+
+    for linea in lista_datos:
+        ingreso = linea.split(',')
+        nombre_registro.append(ingreso[0])
+
+    if persona not in nombre_registro:
+        ahora = datetime.now()
+        string_ahora = ahora.strftime('%H:%M:%S')
+        f.writelines(f'\n{persona}, {string_ahora}')
 
 lista_empleados_codificada = codificar(mis_imagenes)
 
@@ -76,6 +87,9 @@ else:
             cv2.rectangle(imagen, (x1, y2 - 35), (x2, y2), (0,255,0), cv2.FILLED)
             cv2.putText(imagen, nombre, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 2)
 
+            #Llamar a la funcion
+            registrar_ingresos(nombre)
+            
             #Mostrar la imagen obtenida
             cv2.imshow('Imagen web', imagen)
             #Mantener la ventana abierta
